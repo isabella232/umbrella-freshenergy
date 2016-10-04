1e0220815814c9cca1dbefad548d9d69d9955d18
Ben Keith <ben@benlk.com>
2016-10-04 00:01:49 -0400

    Enqueue styles

--- wp-content/themes/freshenergy/functions.php
+++ wp-content/themes/freshenergy/functions.php
@@ -1,5 +1,14 @@
 <?php
 /**
- * Child theme funcitons for the freshenergy theme
+ * Child theme functions for the freshenergy theme
  */
 
+/**
+ * Include compiled style.css
+ */
+function fe_styles() {
+	wp_dequeue_style( 'largo-child-styles' );
+	$suffix = (LARGO_DEBUG)? '' : '.min';
+	wp_enqueue_style( 'fe', get_stylesheet_directory_uri() . '/css/child' . $suffix . '.css' );
+}
+add_action( 'wp_enqueue_scripts', 'fe_styles', 20 );
