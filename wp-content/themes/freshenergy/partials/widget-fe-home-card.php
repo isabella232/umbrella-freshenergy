07bd7ec6eb372988b353195c6e2baadeb6e076b9
Ben Keith <ben@benlk.com>
2017-04-04 18:31:00 -0400

    fix wrong post IDs causing wrong top terms

--- wp-content/themes/freshenergy/partials/widget-fe-home-card.php
+++ wp-content/themes/freshenergy/partials/widget-fe-home-card.php
@@ -4,11 +4,11 @@
 
 if ( has_post_thumbnail() ) { ?>
 	<div class="row-fluid fe-card">
-		<div <?php post_class( $recent->ID ); ?> >
+		<div <?php post_class( $post->ID ); ?> >
 			<a href="<?php echo get_permalink(); ?>" class="blocklink"></a>
 			<a href="<?php echo get_permalink(); ?>"><?php echo the_post_thumbnail( 'large' ); ?></a>
 			<div class="text-wrapper">
-				<h5 class="top-tag"><?php largo_top_term( array( 'post' => $recent->ID ) ); ?></h5>
+				<h5 class="top-tag"><?php largo_top_term( array( 'post' => $post->ID ) ); ?></h5>
 				<h4><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h4>
 				<span class="byline"><?php echo largo_byline(false); ?></span>
 				<?php largo_excerpt( null, 1 ); ?>
@@ -17,10 +17,10 @@ if ( has_post_thumbnail() ) { ?>
 	</div>
 <?php } else { ?>
 	<div class="row-fluid fe-card">
-		<div <?php post_class( $recent->ID ); ?> >
+		<div <?php post_class( $post->ID ); ?> >
 			<a href="<?php echo get_permalink(); ?>" class="blocklink"></a>
 			<div class="text-wrapper">
-				<h5 class="top-tag"><?php largo_top_term( array( 'post' => $recent->ID ) ); ?></h5>
+				<h5 class="top-tag"><?php largo_top_term( array( 'post' => $post->ID ) ); ?></h5>
 				<h4><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h4>
 				<span class="byline"><?php echo largo_byline(false); ?></span>
 				<?php largo_excerpt( null, 1 ); ?>
