aca342ca6d7f181df93a718e337b8ca698194652
Ben Keith <ben@benlk.com>
2017-05-03 17:46:51 -0400

    Style updates on https://github.com/INN/umbrella-freshenergy/pull/3#discussion-diff-114635120R28

--- wp-content/themes/freshenergy/inc/images.php
+++ wp-content/themes/freshenergy/inc/images.php
@@ -20,14 +20,13 @@ add_action( 'the_content', function( $content ) {
  * @since 0.1
  */
 function fe_attachment_image_link_remove_filter( $content ) {
-	$content =
-	preg_replace(
+	$content = preg_replace(
 		// replace nth item in first array with nth item in second array: saves middle
 		array(
 			'{<a(.*?)(?:wp-att|wp-content\/uploads)[^"\']+(?:png|jpeg|jpg|svg|gif)[^>]*><img}',
 			'{ wp-image-[0-9]*" /></a>}'
 		),
-		array('<img','" />'),
+		array( '<img', '" />' ),
 		$content
 	);
 	return $content;
