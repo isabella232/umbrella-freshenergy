c9443d2d233bfd94a9e57ca220ed72461a6c19f1
Ben Keith <ben@benlk.com>
2019-02-13 18:48:53 -0500

    Indentation fixes in functions.php

--- wp-content/themes/freshenergy/functions.php
+++ wp-content/themes/freshenergy/functions.php
@@ -58,22 +58,23 @@ function fe_js() {
 add_action('wp_enqueue_scripts', 'fe_js');
 
 function add_taxonomies_to_pages() {
- register_taxonomy_for_object_type( 'post_tag', 'page' );
- register_taxonomy_for_object_type( 'category', 'page' );
- }
+	register_taxonomy_for_object_type( 'post_tag', 'page' );
+	register_taxonomy_for_object_type( 'category', 'page' );
+}
 add_action( 'init', 'add_taxonomies_to_pages' );
- if ( ! is_admin() ) {
- add_action( 'pre_get_posts', 'category_and_tag_archives' );
- 
- }
+if ( ! is_admin() ) {
+	add_action( 'pre_get_posts', 'category_and_tag_archives' );
+
+}
+
 function category_and_tag_archives( $wp_query ) {
-$my_post_array = array('post','page');
- 
- if ( $wp_query->get( 'category_name' ) || $wp_query->get( 'cat' ) )
- $wp_query->set( 'post_type', $my_post_array );
- 
- if ( $wp_query->get( 'tag' ) )
- $wp_query->set( 'post_type', $my_post_array );
+	$my_post_array = array('post','page');
+
+	if ( $wp_query->get( 'category_name' ) || $wp_query->get( 'cat' ) )
+	$wp_query->set( 'post_type', $my_post_array );
+
+	if ( $wp_query->get( 'tag' ) )
+	$wp_query->set( 'post_type', $my_post_array );
 }
 
 
